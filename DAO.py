e22201309656c3137e5784be5a6559c2a2aaa130
MahmoudKhaled007 <85295806+MahmoudKhaled007@users.noreply.github.com>
2023-08-18 01:03:43 +0300

    Done WIth KWRGS employee and department

--- DAO.py
+++ DAO.py
@@ -70,11 +70,7 @@ class EmployeeDAO:
         self.cursor.execute(query, (emp_id,))
         result = self.cursor.fetchone()
         if result:
-            emp_id, fname, lname, location, phone1, sex, email, password = result
-            employee = Employee(
-                emp_id, fname, lname, location, phone1, sex, email, password
-            )
-            return employee
+            return result
         else:
             return None
 
@@ -104,44 +100,222 @@ class EmployeeDAO:
         self.connection.commit()
         return self.cursor.lastrowid
 
-    def update_emp(self, employees):
-        """Update  multiple records to the Employee table
+    # def update_emp(self, employees):
+    #     """Update  multiple records to the Employee table
+
+    #     Args:
+    #         employees (list): List of Employee objects
+
+    #     Returns:
+    #         int: Number of rows affected
+    #     """
+    #     query = "UPDATE employee SET fname=%s, lname=%s, location=%s, phone1=%s, sex=%s, email=%s, pass=%s WHERE emp_id = %s"
+    #     values = [
+    #         (
+    #             emp.fname,
+    #             emp.lname,
+    #             emp.location,
+    #             emp.phone1,
+    #             emp.sex,
+    #             emp.email,
+    #             emp.password,
+    #             emp.emp_id,
+    #         )
+    #         for emp in employees
+    #     ]
+    #     self.cursor.executemany(query, values)
+    #     self.connection.commit()
+    #     print(self.cursor.rowcount, "record(s) affected")
+
+    def update_emp(self, employees_id, **kwargs):
+        """Update multiple records in the employee table
 
         Args:
-            employees (list): List of Employee objects
+            employees (list): List of employees_ids
+            **kwargs: Additional key-value pairs for updating specific columns
 
         Returns:
             int: Number of rows affected
         """
-        query = "UPDATE employee SET fname=%s, lname=%s, location=%s, phone1=%s, sex=%s, email=%s, pass=%s WHERE emp_id = %s"
-        values = [
-            (
-                emp.fname,
-                emp.lname,
-                emp.location,
-                emp.phone1,
-                emp.sex,
-                emp.email,
-                emp.password,
-                emp.emp_id,
-            )
-            for emp in employees
-        ]
+        # Construct the SET clause dynamically based on kwargs
+        set_clause = ", ".join([f"{key}=%s" for key in kwargs.keys()])
+
+        query = f"UPDATE employee SET {set_clause} WHERE emp_id = %s"
+        values = [tuple(list(kwargs.values()) + [emp]) for emp in employees_id]
+        print(values)
         self.cursor.executemany(query, values)
         self.connection.commit()
         print(self.cursor.rowcount, "record(s) affected")
 
-    def delete_emp(self, emp_id):
-        """Delete  one record to the Employee table
+    def delete_emp(self, emp_ids):
+        """Delete  records to the Employee table
 
         Args:
-            emp_id (int): Id of employee mustn't be a foreign key in another table
+            emp_ids (list): Ids of employees, mustn't be a foreign key in another table
 
         Returns:
             int: Number of rows affected
         """
         query = "DELETE FROM employee WHERE emp_id = %s"
-        self.cursor.execute(query, (emp_id,))
+        values = [([emp]) for emp in emp_ids]
+        self.cursor.executemany(query, values)
         self.connection.commit()
         print(self.cursor.rowcount, "record(s) affected")
 
+
+class Department:
+    def __init__(self, dep_id=None, name=None, salary_range=None, description=None):
+        self.dep_id = dep_id
+        self.name = name
+        self.salary_range = salary_range
+        self.description = description
+
+
+class DepartmentDAO:
+    """
+    _summary_
+    This class for to represent Database Department Tables and provides the encapsulation of the database-specific code,
+    that is, it is isolated from the main program.
+    To achieve principle of Separation of Logic and it make it easy when testing
+    """
+
+    def __init__(self):
+        """_summary_
+        When the class being called every time will open a new connection to the database
+        """
+        self.connection = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="mahmoud2001",
+            database="employee_managment_system",
+        )
+        self.cursor = self.connection.cursor(buffered=True)
+
+    def get_all_departments(self):
+        """
+            Select Statement to retrieve all data for the employees
+        Returns:
+            _type_: _description_
+            Object: which contains Record data
+        """
+        query = "SELECT dep_id,name,salary_range, description FROM department"
+        self.cursor.execute(query)
+        result = self.cursor.fetchall()
+        if result:
+            [print(i) for i in result]
+            return result
+        else:
+            return None
+
+    def get_dep_by_id(self, dep_id):
+        """
+            Select Statement to retrieve all data for the Departments based on their id
+        Args:
+            dep_id (_type_): _description_
+                dep_id (int): id of employee needs to retrieve
+        Returns:
+            _type_: _description_
+            Object: which contains Record data
+        """
+        query = "SELECT dep_id,name,salary_range, description FROM department WHERE dep_id = %s"
+        self.cursor.execute(query, (dep_id,))
+        result = self.cursor.fetchone()
+        if result:
+            return result
+        else:
+            return None
+
+        # def insert_dep(self):
+        """Insert multiple records to the Department table
+
+        Args:
+            departments (list): List of department objects
+
+        Returns:
+            int: The last inserted row ID
+        """
+        query = "INSERT INTO department (dep_id,name,salary_range, description) VALUES (%s,%s, %s,%s)"
+        values = [
+            (
+                dep.dep_id,
+                dep.name,
+                dep.salary_range,
+                dep.description,
+            )
+            for dep in departments
+        ]
+        self.cursor.executemany(query, values)
+        self.connection.commit()
+        return self.cursor.lastrowid
+
+    def insert_dep(self, **kwargs):
+        """Insert a record into the Department table
+
+        Args:
+            **kwargs: Key-value pairs for inserting specific columns
+
+        Returns:
+            int: The last inserted row ID
+        """
+        column_names = ", ".join(kwargs.keys())
+        value_placeholders = ", ".join(["%s" for _ in kwargs.keys()])
+        query = f"INSERT INTO department ({column_names}) VALUES ({value_placeholders})"
+        values = tuple(kwargs.values())
+        self.cursor.execute(query, values)
+        self.connection.commit()
+        return self.cursor.lastrowid
+
+        # def update_dep(self, departments):
+        """Update  multiple records to the Department table
+
+        Args:
+            departments (list): List of department objects
+
+        Returns:
+            int: Number of rows affected
+        """
+        # DONE add What valeus to update maybe we can use **kwrgs
+        query = "UPDATE department SET  name=%s, salary_range=%s, description=%s WHERE dep_id = %s"
+        values = [
+            (dep.name, dep.salary_range, dep.description, dep.dep_id)
+            for dep in departments
+        ]
+        print(values)
+        self.cursor.executemany(query, values)
+        self.connection.commit()
+        print(self.cursor.rowcount, "record(s) affected")
+
+    def update_dep(self, departments, **kwargs):
+        """Update multiple records in the Department table
+
+        Args:
+            departments (list): List of department objects
+            **kwargs: Additional key-value pairs for updating specific columns
+
+        Returns:
+            int: Number of rows affected
+        """
+        # Construct the SET clause dynamically based on kwargs
+        set_clause = ", ".join([f"{key}=%s" for key in kwargs.keys()])
+
+        query = f"UPDATE department SET {set_clause} WHERE dep_id = %s"
+        values = [tuple(list(kwargs.values()) + [dep]) for dep in departments]
+        print(values)
+        self.cursor.executemany(query, values)
+        self.connection.commit()
+        print(self.cursor.rowcount, "record(s) affected")
+
+    def delete_dep(self, dep_ids):
+        """Delete  records to the Department table
+
+        Args:
+            dep_ids (list): Ids of Departments, mustn't be a foreign key in another table
+
+        Returns:
+            int: Number of rows affected
+        """
+        query = "DELETE FROM department WHERE dep_id = %s"
+        values = [([dep]) for dep in dep_ids]
+        self.cursor.executemany(query, values)
+        self.connection.commit()
+        print(self.cursor.rowcount, "record(s) affected")
--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 import DAO
 
-EmployeeDAO = DAO.EmployeeDAO()
+# Employee
+# EmployeeDAO = DAO.EmployeeDAO()
 
-# Retrieve a user by ID
-user = EmployeeDAO.get_emp_by_id(1)
-if user:
-    print(f"User: ID={user.emp_id}, Name={user.fname}, Email={user.email}")
-else:
-    print("User not found.")
+# # Retrieve a user by ID
+# user = EmployeeDAO.get_emp_by_id(1)
+# if user:
+#     print(f"User: ID={user.emp_id}, Name={user.fname}, Email={user.email}")
+# else:
+#     print("User not found.")
 
-# emp_dao = DAO.EmployeeDAO()  # Create an instance of the EmployeeDAO class
-# emp = DAO.Employee(
-#     None, "Zidan", "Mohamed", "New Cairo", "01065216622", "M", "zidan@gmail.com", "123"
-# )
-# print(emp_dao.insert_emp(emp))
+# # emp_dao = DAO.EmployeeDAO()  # Create an instance of the EmployeeDAO class
+# # emp = DAO.Employee(
+# #     None, "Zidan", "Mohamed", "New Cairo", "01065216622", "M", "zidan@gmail.com", "123"
+# # )
+# # print(emp_dao.insert_emp(emp))
 
 emp_dao = DAO.EmployeeDAO()
 emp1 = DAO.Employee(
@@ -23,15 +24,58 @@ emp2 = DAO.Employee(
     1, "qqq", "Doe", "London", "0123456789", "M", "john.doe@gmail.com", "456"
 )
 
-employees = [emp1, emp2]
+employees = ["1", "2", "3"]
 
-# # last_row_id = emp_dao.insert_emp(employees)
+# last_row_id = emp_dao.insert_emp(employees)
 
-# # print(last_row_id)
-# print(emp2.emp_id)
-# x = emp_dao.update_emp(employees)
+# print(last_row_id)
+
+# x = emp_dao.update_emp(employees, fname="KWRGS", lname="DONEEEE")
 # print(x)
+# DELETE
+# emp_dao.get_all_employees()
+# empids = ["18", "19"]
+# emp_dao.delete_emp(empids)
+# emp_dao.get_all_employees()
+# #TODO Depa
+dep_obj = DAO.DepartmentDAO()
+# dep_obj.get_all_departments()
 
-empids = [4, 5, 6]
-# emp_dao.delete_emp(6)
-emp_dao.get_all_employees()
+# de0partment = dep_obj.get_dep_by_id(2)
+# if department:
+#     print(department)
+# else:
+#     print("department not found.")
+
+# emp1 = DAO.Employee(
+#     2, "eee", "Mohamed", "New Cairo", "01065216622", "M", "zidan@gmail.com", "123"
+# )
+# emp2 = DAO.Employee(
+#     1, "qqq", "Doe", "London", "0123456789", "M", "john.doe@gmail.com", "456"
+# )
+
+# employees = [emp1, emp2]
+
+# # # last_row_id = emp_dao.insert_emp(employees)
+
+# Insert into
+# dep1 = DAO.Department(5, "Data Science", "20000-25000", "Updated from python2")
+# UPDATEEEEE
+# # dep2 = DAO.Department(8, "Data Engineering", "20000-25000", "Updated from python4")
+# departments = ["1", "2", "3"]
+# # last_row_id = dep_obj.insert_dep(departments)
+
+# x = dep_obj.update_dep(departments, name="Kwrgsssssssss", description="from KWRGS")
+# print(x)
+# dep_obj.get_all_departments()
+
+# Insert into KWRGS
+dep1 = DAO.Department(5, "Data Science", "20000-25000", "Updated from python2")
+
+last_row_id = dep_obj.insert_dep(
+    name="INSERT_KWRGS3", salary_range="1000-5000", description="HEYYYY from KWRGS"
+)
+# DELETEEE
+# dep_obj.get_all_departments()
+x = ["11", "10"]
+dep_obj.delete_dep(x)
